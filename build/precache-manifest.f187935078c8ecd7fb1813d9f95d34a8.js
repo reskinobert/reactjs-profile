95661229552b9dfadfd4cb96908b270302ee3a51
reskinobert <115645104+reskinobert@users.noreply.github.com>
2023-08-19 00:40:59 +0700

    Add files via upload

---
+++ build/precache-manifest.f187935078c8ecd7fb1813d9f95d34a8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "49fdbd98feae064e5e86",
+    "url": "/static/css/main.18126466.chunk.css"
+  },
+  {
+    "revision": "49fdbd98feae064e5e86",
+    "url": "/static/js/main.49fdbd98.chunk.js"
+  },
+  {
+    "revision": "f4ad8aa417c1c82bd0b0",
+    "url": "/static/css/1.7c37e113.chunk.css"
+  },
+  {
+    "revision": "f4ad8aa417c1c82bd0b0",
+    "url": "/static/js/1.f4ad8aa4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8a8b392ed19f5af2ed17820c58f9ff8f",
+    "url": "/static/media/portfolio1.8a8b392e.png"
+  },
+  {
+    "revision": "fb14d5410b9ce79594656b6384da9fdc",
+    "url": "/static/media/cv.fb14d541.pdf"
+  },
+  {
+    "revision": "3fed0814a9eb031529d5ae1baf94b38f",
+    "url": "/static/media/me2.3fed0814.png"
+  },
+  {
+    "revision": "d07c6de1a74cf884967040b28847a157",
+    "url": "/static/media/machine-learning.d07c6de1.png"
+  },
+  {
+    "revision": "a3c7327c88b60dcd915041efcca04639",
+    "url": "/static/media/data-transfer.a3c7327c.png"
+  },
+  {
+    "revision": "908ef9bd0eb80acccb5b6f6ae8134841",
+    "url": "/static/media/cmyk.908ef9bd.png"
+  },
+  {
+    "revision": "c071c486216d7059c5248a6e29f40d0f",
+    "url": "/static/media/noise-1.c071c486.png"
+  },
+  {
+    "revision": "30499da4a8280812a3241bbfcd8ed9e9",
+    "url": "/index.html"
+  }
+];
